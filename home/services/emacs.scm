47b0a0aa8fb303fe4fb7e7202bdc306a08c58f68
bfh <bfh@snail.mail>
2025-03-04 18:22:33 -0500

    fix emacs

--- home/services/emacs.scm
+++ home/services/emacs.scm
@@ -1,7 +1,12 @@
+
 (define-module (home services emacs)
   #:use-module (gnu)
   #:use-module (gnu home)
   #:use-module (gnu home services)
+  #:use-module (gnu packages aspell)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages emacs-xyz)
   #:use-module (home packages emacs)
@@ -10,37 +15,75 @@
 (define config-dir (string-append (getenv "HOME") "/dots/home/config"))
 
 (define (home-emacs-config-profile-service config)
-  (list emacs-next-pgtk       ;; Changed from emacs to emacs-next-pgtk since that's what you use
+  (list emacs-next-pgtk
 	emacs-geiser
-	emacs-ef-themes
+	;; UI Enhancements
+        emacs-ef-themes
 	emacs-doom-themes
-	emacs-vertico
-	emacs-vertico-posframe
-	emacs-corfu
-	emacs-orderless
-	emacs-embark
+	emacs-catppuccin-theme
+        emacs-which-key
+        emacs-which-key-posframe
+        emacs-posframe
+        emacs-doom-modeline
+        emacs-mini-echo
+        emacs-kind-icon
+        emacs-nerd-icons
+;;        emacs-nerd-icons-completion
+;;        emacs-vscode-icon
+;;        emacs-ligature
+;;        emacs-breadcrumb
+;;        emacs-ultra-scroll
+        ;; Completion
+;;        emacs-vertico 
+;;        emacs-orderless
+;;        emacs-consult
+;;        emacs-marginalia
+;;        emacs-corfu
+;;        emacs-cape
+	;; Evil & Window Management
+        emacs-evil
+        emacs-evil-collection
+        emacs-undo-fu
+        emacs-undo-fu-session
+        emacs-vundo
+	;; Dashboard & File Management
+;;        emacs-grid not in guix
+        emacs-enlight
+        emacs-dirvish
+	;;        emacs-dired-subtree
+	;; Development Tools
+        emacs-magit
+        emacs-transient
+        emacs-git-gutter
+        emacs-git-gutter-fringe
+;;        emacs-tree-sitter-langs
+        emacs-dtrt-indent
+        emacs-suggest
+        emacs-eros
 	emacs-dash
-	emacs-mini-echo
 	emacs-guix
 	emacs-eat
 	emacs-geiser-guile
-	emacs-evil
 	emacs-lispy
 	emacs-lispyville
 	emacs-keycast
-	emacs-evil-collection
-	emacs-vundo
-;;	emacs-vim-tab-bar
-	emacs-cape
-	emacs-marginalia
+	emacs-gptel
 	emacs-git-gutter
 	emacs-git-gutter-fringe
+	emacs-arei
+	;; Programming Language Support
+        emacs-rustic
+        emacs-go-mode
+        emacs-lua-mode
+        emacs-fennel-mode
+        emacs-terraform-mode
+        emacs-cider
+        emacs-web-mode
+        emacs-nix-mode
+        emacs-ansible
+;;        emacs-python
+        emacs-flymake-collection
 ;;	emacs-tabspaces
-	emacs-kind-icon
-	emacs-nerd-icons
-	emacs-all-the-icons
-	emacs-all-the-icons-dired
-	emacs-all-the-icons-completion
 	emacs-rainbow-delimiters
 	emacs-ligature
 	emacs-highlight
@@ -48,14 +91,6 @@
 	emacs-highlight-sexp
 	emacs-highlight-numbers
 	emacs-highlight-escape-sequences
-	emacs-rustic
-	emacs-go-mode
-	emacs-lua-mode
-	emacs-terraform-mode
-	emacs-fennel-mode
-	emacs-cider
-;;	emacs-css
-	emacs-web-mode
 	emacs-sly
 	emacs-sly-asdf
 	emacs-ansible
@@ -64,7 +99,8 @@
 	emacs-dirvish
 ;;	emacs-dired-subtree
 	emacs-magit
-	emacs-super-save))
+	))
+;;	emacs-super-save))
 
 (define (home-emacs-config-files-service config)
     `(("emacs/early-init.el"
@@ -87,4 +123,3 @@
 	   home-xdg-configuration-files-service-type
 	   home-emacs-config-files-service)))
    (default-value #t)))
-
--- home/users/bfh.scm
+++ home/users/bfh.scm
@@ -16,7 +16,10 @@
   #:use-module (gnu packages glib)
   #:use-module (gnu services)
   #:use-module (home services bash)
-  #:use-module (home services emacs))
+  #:use-module (home services emacs)
+;;  #:use-module (home services home-impure-symlinks)
+;;  #:use-module (home services mutable-files)
+  )
 
 (define config-dir (string-append (getenv "HOME") "/dots/home/config"))
 
@@ -24,59 +27,158 @@
  (packages
   (specifications->packages
    (list "guile"
-	 ;;	 "emacs-treesitter-langs"
-	 ;;	 "emacs-shr-tag-pre-highlight"
-	 ;;	 "dot"
-	 ;;"eww"
-	 "alacritty"
-	 "mako"
-	 "libnotify"
-	 "wofi"
-	 "iproute2"
-	 "cmake"
-	 "git"
-	 "ripgrep"
-	 "fd"
-	 "jq"
-	 "awscliv2"
-	 "font-jetbrains-mono"
-	 "font-liberation"
-	 "font-dejavu"
-	 "font-google-noto"
-	 "adwaita-icon-theme"
-	 "gnome-themes-extra"
-	 "htop"
-	 "google-chrome-stable"
-	 "tmux"
-	 "curl"
-	 "wget")))
+         "guile-colorized"
+         "guile-readline"
+         "coreutils"
+         "alacritty"
+         ;;"eww" not yet
+         "nushell"
+         "xz"
+         "make"
+         "ncurses"
+         "pkg-config"
+         "vim"
+         "mako"
+         "libnotify"
+         "slurp"
+         "grimshot"
+         "wl-clipboard"
+         "wofi"
+         "git"
+         "ripgrep"
+         "cl-trial"
+         "sbcl-trial"
+         "fd"
+         "jq"
+         "awscliv2"
+         "font-spleen"
+         "font-fira-code"
+         "font-jetbrains-mono"
+         "font-liberation"
+         "font-dejavu"
+         "font-google-noto"
+         "font-terminus"
+
+         "adwaita-icon-theme"
+         "gnome-themes-extra"
+         "htop"
+         "google-chrome-stable"
+         ;;"dot"
+         "tmux"
+         "curl"
+         "wget"
+
+         "llvm@15"
+         "gopls"
+         "ruby-solargraph"
+         "gdb"
+         "zlib"
+         "sbcl"
+         "sbcl-slynk"
+         "bind"
+         "unzip"
+         ;;"wine"
+         "zip"
+         "godot"
+         "rust-analyzer"
+         "rust:cargo"
+         "node"
+         "clojure-tools"
+         "clojure"
+         "vlc"
+         "mosh"
+         "rust"
+         "elixir"
+         "python"
+         "automake"
+         "autoconf"
+         "perl"
+         "openjdk@18.0.2:jdk"
+
+         "tree-sitter-python"
+         "tree-sitter-markdown"
+         "tree-sitter-scheme"
+         "tree-sitter-typescript"
+         "tree-sitter-javascript"
+         "tree-sitter-rust"
+         "tree-sitter-ruby"
+         "tree-sitter-html"
+         "tree-sitter-php"
+         "tree-sitter-org"
+         "tree-sitter-json"
+         "tree-sitter-java"
+         "tree-sitter-haskell"
+         "tree-sitter-css"
+         "tree-sitter-go"
+         "tree-sitter-dockerfile"
+         "tree-sitter-elixir"
+         "tree-sitter-clojure"
+         "tree-sitter-cpp"
+         "tree-sitter-c"
+         "tree-sitter"
+
+         "imagemagick"
+
+         "steam"
+         "gimp"
+         "aseprite"
+         "aspell-dict-en"
+         "hunspell-dict-en-us"
+         "hunspell"
+         "ispell"
+         "aspell"
+         "leiningen"
+         "libvterm"
+         "pavucontrol"
+
+         "mupen64plus-ui-console"
+         "mupen64plus-core"
+         "mupen64plus-audio-sdl"
+         "mupen64plus-input-sdl"
+         "mupen64plus-video-z64"
+         "sicp"
+         "sbcl"
+	 "lem"
+	 "tiled"
+         "gammastep"
+         "guile-ares-rs"
+         "retroarch-assets"
+         "libretro-mupen64plus-nx"
+         "retroarch"
+
+         "flatpak"
+
+         )))
 
  (services
   (list
    (service home-xdg-configuration-files-service-type
-	    `(("sway/config"
-	       ,(local-file (string-append config-dir "/sway/.config/sway")))
-	      ("eww/eww.yuck"
-	       ,(local-file (string-append config-dir "/eww/eww.yuck")))
-	      ("eww/eww.scss"
-	       ,(local-file (string-append config-dir "/eww/eww.scss")))
+            `(("sway/config"
+               ,(local-file (string-append config-dir "/sway/.config/sway")))
+              ("eww/eww.yuck"
+               ,(local-file (string-append config-dir "/eww/eww.yuck")))
+              ("eww/eww.scss"
+               ,(local-file (string-append config-dir "/eww/eww.scss")))
               ("alacritty/alacritty.toml"
                ,(local-file (string-append config-dir "/alacritty/.config/alacritty/alacritty.toml")))
               ("wezterm/wezterm.lua"
                ,(local-file (string-append config-dir "/wezterm/.config/wezterm/wezterm.lua")))
-	      ("waybar/config"
-	       ,(local-file (string-append config-dir "/waybar/waybar")))
-	      ("wofi/style.css"
-	       ,(local-file (string-append config-dir "/wofi/style.css")))
-	      ("waybar/style.css"
-	       ,(local-file (string-append config-dir "/waybar/style.css")))))
+              ("waybar/config"
+               ,(local-file (string-append config-dir "/waybar/waybar")))
+              ("wofi/style.css"
+               ,(local-file (string-append config-dir "/wofi/style.css")))
+              ("waybar/style.css"
+               ,(local-file (string-append config-dir "/waybar/style.css")))
+              ("common-lisp/source-registry.conf.d/guix.conf"
+               ,(local-file (string-append config-dir "/common-lisp/source-registry.conf.d/guix.conf")))))
+;;   (service home-mutable-files-service-type)
 
-	    (home-bash-service #:config-dir config-dir)
-	    (service home-dbus-service-type)
-	    (service home-emacs-config-service-type)
-	    (service home-pipewire-service-type)
-	    (service home-ssh-agent-service-type)
-	    (service home-openssh-service-type
-		     (home-openssh-configuration
-		      (add-keys-to-agent "yes")))
-	    )))
+   (home-bash-service #:config-dir config-dir)
+   (service home-dbus-service-type)
+   (service home-emacs-config-service-type)
+   (service home-pipewire-service-type)
+   (service home-ssh-agent-service-type)
+   (service home-openssh-service-type
+            (home-openssh-configuration
+             (add-keys-to-agent "yes")))
+   )))
