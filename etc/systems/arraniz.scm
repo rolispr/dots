c9929ea97d93d1045a2a09b3999f0fb7c2590e42
bfh <bfh@snail.mail>
2025-03-06 23:47:21 -0500

    Revert "brave new world"
    This reverts commit 815a375e72c6101ced55166604d92cb3cfc66d22.

--- etc/systems/arraniz.scm
+++ etc/systems/arraniz.scm
@@ -1,21 +1,16 @@
 (define-module (etc systems arraniz)
   #:use-module (gnu)
   #:use-module (guix)
-  #:use-module (guix gexp)
-  #:use-module (guix packages)
-  #:use-module (guix utils)
-
-  #:use-module (gnu packages certs)
+  #:use-module (etc systems base-system)
+  #:use-module (gnu system nss)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages wm)
-  #:use-module (gnu packages display-managers)
   #:use-module (gnu packages admin)
-  #:use-module (gnu packages audio)
+  #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages terminals)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages image)
   #:use-module (gnu packages xorg)
-  #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages fonts)
@@ -23,24 +18,15 @@
   #:use-module (gnu packages ssh)
   #:use-module (gnu packages lisp)
   #:use-module (gnu packages lisp-xyz)
-  #:use-module (gnu packages linux) ;; pipewire
-
   #:use-module (gnu services desktop)
   #:use-module (gnu services dbus)
   #:use-module (gnu services ssh)
-  #:use-module (gnu services sddm)
-  #:use-module (gnu services xorg)
-  #:use-module (gnu services networking)
-  #:use-module (gnu services web)
-  ;;#:use-module (gnu services base)
-
-  #:use-module (gnu system nss)
-
   #:use-module (nongnu packages linux)
-  #:use-module (nongnu system linux-initrd)
-
-  #:use-module (etc systems base-system)
+  #:use-module (gnu services base)
   #:use-module (etc packages stumpwm))
+
+(use-service-modules desktop dbus networking xorg)
+(use-package-modules wm fonts linux)
 
 (define %stumpwm-packages
   (list sbcl
@@ -72,113 +58,102 @@
 
 (define %guixos-system-packages
   (list openssh
-        sway
-        waybar
-        swaylock
-        swayidle
-        pipewire
-        wireplumber
-        network-manager
-        network-manager-applet
-        brightnessctl
-        wlgreet
-        xorg-server-xwayland
-        xdg-desktop-portal
-        xdg-desktop-portal-wlr))
-
+	sway
+	waybar
+	swaylock
+	swayidle
+	pipewire
+	wireplumber
+	network-manager
+	network-manager-applet
+	brightnessctl
+	wlgreet
+	xorg-server-xwayland
+	xdg-desktop-portal
+	xdg-desktop-portal-wlr))
+	
 (define %guixos-base-packages
   (append %stumpwm-packages
-          %guixos-system-packages
-          %base-packages))
+	  %guixos-system-packages
+	  %base-packages))
 
 (operating-system
- ;; (inherit base-system)
+ (inherit base-system)
  (host-name "arraniz")
- (locale "en_US.utf8")
- (timezone "America/New_York")
- (keyboard-layout (keyboard-layout "us"))
 
  (kernel linux)
- (initrd microcode-initrd)
- (firmware (list iwlwifi-firmware sof-firmware linux-firmware))
+ (firmware (list linux-firmware))
+ (keyboard-layout base-keyboard-layout)
  (bootloader (bootloader-configuration
-              (bootloader grub-efi-bootloader)
-              (targets '("/boot/efi"))
-              (keyboard-layout base-keyboard-layout)))
+	      (bootloader grub-efi-bootloader)
+	      (targets '("/boot/efi"))
+	      (keyboard-layout base-keyboard-layout)))
  (file-systems (append
-                (list (file-system
-                       (device (file-system-label "root"))
-                       (mount-point "/")
-                       (type "ext4"))
-                      (file-system
-                       (device (uuid "D140-4BF5" 'fat))
-                       (mount-point "/boot/efi")
-                       (type "vfat")))
-                %base-file-systems))
+		(list (file-system
+		       (device (file-system-label "root"))
+		       (mount-point "/")
+		       (type "ext4"))
+		      (file-system
+		       (device (uuid "D140-4BF5" 'fat))
+		       (mount-point "/boot/efi")
+		       (type "vfat")))
+		%base-file-systems))
  (swap-devices (list (swap-space
-                      (target (uuid "a0bca027-0738-4287-933b-42f5960a25ed")))))
- (users (cons* (user-account
-                (name "bfh")
-                (comment "some guy")
-                (group "users")
-                (home-directory "/home/bfh")
-                (supplementary-groups '("tty" "lp" "wheel" "netdev" "audio" "video")))
-               %base-user-accounts))
+		      (target (uuid "a0bca027-0738-4287-933b-42f5960a25ed")))))
+ (users (cons (user-account
+	       (name "bfh")
+	       (comment "some guy")
+	       (group "users")
+	       (supplementary-groups '("wheel" "netdev" "audio" "video")))
+	      %base-user-accounts))
 
- (packages (append
-            (list (specification->package "openssh")
-                  (specification->package "sway")
-                  (specification->package "waybar")
-                  (specification->package "swaylock")
-                  (specification->package "swayidle")
-                  (specification->package "pipewire")
-                  (specification->package "network-manager")
-                  (specification->package "network-manager-applet")
-                  (specification->package "brightnessctl")
-                  (specification->package "wlgreet")
-                  (specification->package "xorg-server-xwayland")
-                  (specification->package "xdg-desktop-portal-wlr")
-                  (specification->package "xdg-desktop-portal")
-                  (specification->package "wireplumber")
-                  (specification->package "alacritty")
-                  (specification->package "git"))
-            %stumpwm-packages
-            %base-packages))
- (services (modify-services (cons (service gnome-desktop-service-type)
-                                  %desktop-services)
-                            ;; Get nonguix substitutes.
-                            (guix-service-type config =>
-                                               (guix-configuration
-                                                (inherit config)
-                                                (substitute-urls
-                                                 (append (list "https://substitutes.nonguix.org")
-                                                         %default-substitute-urls))
-                                                (authorized-keys
-                                                 (append (list (local-file "./nonguix-signing-key.pub"))
-                                                         %default-authorized-guix-keys))))))
+ (packages %guixos-base-packages)
+
+ (services
+  (append
+   (modify-services %base-services
+		    (delete console-font-service-type)
+		    (delete login-service-type)
+		    (delete mingetty-service-type)
+		    (delete agetty-service-type)
+		    )
+   (list
+    (service network-manager-service-type)
+    (service wpa-supplicant-service-type)
+    (service elogind-service-type)
+    (service ntp-service-type)
+    (service openssh-service-type
+	     (openssh-configuration
+	      (permit-root-login #f)
+	      (allow-empty-passwords? #f)
+	      (password-authentication? #t)
+	      (max-connections 4)
+;;	      (port-number 10111)
+	      (x11-forwarding? #f)
+	      (allow-agent-forwarding? #t)))
+    (service upower-service-type)
+    (service udisks-service-type)
+    (service polkit-service-type)
+    (service dbus-root-service-type)
+    (udev-rules-service 'brightnessctl brightnessctl)
+    (service console-font-service-type
+	     (map (lambda (tty)
+		    (cons tty (file-append
+			       font-terminus
+			       "/share/consolefonts/ter-132n")))
+		  '("tty1" "tty2" "tty3")))
+    (service greetd-service-type
+	     (greetd-configuration
+	      (greeter-supplementary-groups
+	       (list "users" "video" "input"))
+	      (terminals
+	       (list
+		(greetd-terminal-configuration
+		 (terminal-vt "1")
+		 (terminal-switch #t)
+		 (default-session-command (greetd-wlgreet-sway-session)))
+		(greetd-terminal-configuration (terminal-vt "2"))
+		(greetd-terminal-configuration (terminal-vt "3")))))))))
+
  ;; Allow resolution of '.local' host names with mDNS.
  (name-service-switch %mdns-host-lookup-nss))
-
-;;		     man-pages
-;;		     openssh
-;;		     pwgen
-;;		     unzip
-;;		     zip
-;;		     aspell
-;;		     aspell-dict-en
-;;		     mpv
-;;		     mpd-mpc
-;;		     asla-utils
-;;		     curl
-;;		     tree
-;;		     msmtp
-;;		     isync
-;;		     git
-;;		     make
-;;		     gcc-toolchain
-;;		     gvfs
-;;		     htop
-;;		     wget
-;;		     curl
-;;		     wl-clipboard
-;;		     wofi
