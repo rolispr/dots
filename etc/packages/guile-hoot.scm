cd520554b6dc150df4e1fe96eb066b9356808542
bret <bretfhorne@gmail.com>
2026-05-03 15:22:05 -0400

    etc/packages: add guile-hoot-latest (pinned to main commit 54b0b68)
    Hoot 0.8.0 in upstream Guix master is broken with Guile 3.0.11:
    match-error on mutable-bytevector? in hoot/backend.scm:2039.
    Upstream fix is in main commit cb686c3 (Wingo, 2026-04-03), not yet
    in any tagged release. Inheriting from upstream guile-hoot but
    pointing at main commit 54b0b68 picks up the fix.

---
+++ etc/packages/guile-hoot.scm
@@ -0,0 +1,27 @@
+(define-module (etc packages guile-hoot)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages guile-xyz))
+
+;; Hoot 0.8.0 in upstream Guix master is broken with Guile 3.0.11
+;; (match-error on `mutable-bytevector?` in hoot/backend.scm:2039).
+;; Upstream fix: cb686c3 "Fix Hoot for Guile 3.0.11" (2026-04-03, Wingo).
+;; Pinning to main commit 54b0b68 (2026-04-23) which includes the fix.
+(define %hoot-commit "54b0b68fa7ec246d538e27384d8f6cd02ce89f80")
+(define %hoot-revision "1")
+
+(define-public guile-hoot-latest
+  (package
+    (inherit guile-hoot)
+    (name "guile-hoot")
+    (version (git-version "0.8.0" %hoot-revision %hoot-commit))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://codeberg.org/spritely/hoot.git")
+             (commit %hoot-commit)))
+       (file-name (git-file-name "guile-hoot" version))
+       (sha256
+        (base32 "003ha3nq5sfb346ky61v2zxkca8w7189z8fn0g76l9xw71vs5pw4"))))))
